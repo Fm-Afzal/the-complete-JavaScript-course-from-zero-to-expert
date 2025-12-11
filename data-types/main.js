f775a269d9e32c76108a6a99f82bbe7408ca7694
Afzal <fmafzalulbare@gmail.com>
2025-12-11 16:03:34 +0600

    data types

---
+++ data-types/main.js
@@ -0,0 +1,52 @@
+/*JavaScript has 8 Datatypes:
+
+1. String : --> Sequence of characters, used for text.
+
+   Ex.
+     let firstName = "Afzal";
+
+2. Number : --> Floating point numbers, used for decimals and integers.
+
+Ex.
+  let age = 23;
+
+3. Boolean : --> Logical datatype that can only be true or false.
+
+Ex.
+  let isJavaScriptFun = true;
+
+
+4. Null: --> Represents 'no value' or 'empty value'.
+
+Ex.
+  let emptyValue = null;
+
+5. Undefined: --> A variable that has been declared but not assigned a value.
+
+Ex.
+  let children;
+
+6. Symbol(ES2015): --> Value that is unique and cannot be changed [not useful for now]
+Ex.
+  let sym = Symbol();
+
+7. BigInt: --> Larger integers than the Number type can hold.
+Ex.
+  let bigIntNum = 1234567890123456789012345678901234567890n;
+
+8. Object: --> Collection of properties.
+Ex.
+  let person = {
+    name: "Afzal",
+    age: 23
+  };
+*/
+
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof javaScriptIsFun);
+
+javaScriptIsFun = "YES!";
+console.log(typeof javaScriptIsFun);
