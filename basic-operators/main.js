deb99855992acd341e436847d24fb45a8c1f7b6c
Afzal <fmafzalulbare@gmail.com>
2025-12-18 16:17:27 +0600

    add index.html and main.js files for Basic Operators section

---
+++ basic-operators/main.js
@@ -0,0 +1,26 @@
+const now = 2025;
+const ageAfzal = now - 2000;
+const sagor = now - 2009;
+
+console.log(ageAfzal, sagor);
+
+console.log(ageAfzal * 2, ageAfzal / 2, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Afzal';
+const lastName = 'Bare';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+x--;
+console.log(x);
+
+
+// Comparison operators
+console.log(ageAfzal > sagor); // >, <, >=, <=
+console.log(ageAfzal <=18);
