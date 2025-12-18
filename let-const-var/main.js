b7aa9f8833375084913875674deb69c7552cb0db
Afzal <fmafzalulbare@gmail.com>
2025-12-18 13:01:07 +0600

    add index.html and main.js files for JavaScript fundamentals

---
+++ let-const-var/main.js
@@ -0,0 +1,16 @@
+/*
+let age = 30;
+age = 31;
+console.log(age);
+*/
+
+/*
+const birthYear = 1993;
+birthYear = 1994;
+
+console.log(birthYear);
+*/
+
+var job = 'programmer';
+job = 'teacher';
+console.log(job);
