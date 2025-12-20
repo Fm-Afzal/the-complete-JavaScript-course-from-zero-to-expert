f0f71183a29fe627158d49188413c88a0e121333
Afzal <fmafzalulbare@gmail.com>
2025-12-20 13:17:42 +0600

    add index.html and main.js files for Operator Precedence section

---
+++ operator precedence/main.js
@@ -0,0 +1,13 @@
+const now = 2025;
+const ageAfzal = now - 2000;
+const ageSagor = now - 2009;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; //x=y=10, x=10
+console.log(x, y);
+
+const averageAge = (ageAfzal + ageSagor) / 2;
+
+console.log(ageAfzal, ageSagor, averageAge);
