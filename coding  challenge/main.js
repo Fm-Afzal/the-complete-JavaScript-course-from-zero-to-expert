1f6fd0f2345aeddf437db57b4d470e7edfe832ac
Afzal <fmafzalulbare@gmail.com>
2025-12-26 10:38:46 +0600

    refactor coding challenge #1 and #2 for clarity and consistency

--- coding  challenge/main.js
+++ coding  challenge/main.js
@@ -17,7 +17,9 @@ GOOD LUCK
 
 */
 
+// Codeing challenge #1
 
+/*
 const massMark = 78; // in kg
 const heightMark = 1.69; // in meters
 const massJohn = 92; // in kg
@@ -27,4 +29,28 @@ const BMIMark = massMark / heightMark ** 2;
 const BMIJohn = massJohn / (heightJohn * heightJohn);
 const markHigherBMI = BMIMark > BMIJohn;
 
-console.log(BMIMark, BMIJohn, markHigherBMI);  
+console.log(BMIMark, BMIJohn, markHigherBMI);
+*/
+
+// Codeing challenge #2
+
+const massMark = 95; // in kg
+const heightMark = 1.88; // in meters
+const massJohn = 85; // in kg
+const heightJohn = 1.76; // in meters
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+    // console.log("Mark's BMI is higher than John's!");
+
+    //es6 template literal
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+
+} else {
+    // console.log("John's BMI is higher than Mark's!");
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+}
