68d750a6b9ef03dcd54975da2dd04702e91ba23e
Afzal <fmafzalulbare@gmail.com>
2025-12-29 14:33:35 +0600

    add index.html and main.js files for Equality Operators section

---
+++ equality-operators/main.js
@@ -0,0 +1,13 @@
+const age = "18" //18;
+if (age === 18) console.log('You just became an adult :D (strict)');
+
+if (age == 18) console.log('You just became an adult :D (loose)');
+
+//
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) { 
+    console.log('Cool! 23 is an amazing number!');
+}
