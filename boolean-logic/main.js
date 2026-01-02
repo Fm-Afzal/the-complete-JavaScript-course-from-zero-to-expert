c97b34bea9dee37cdc198374c6407db2c5d1375e
Afzal <fmafzalulbare@gmail.com>
2026-01-02 14:25:56 +0600

    add index.html and main.js files for Boolean Logic section

---
+++ boolean-logic/main.js
@@ -0,0 +1,30 @@
+//Boolean logic
+
+const hasDriversLicense = false; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+//
+
+// const shouldDrive = hasDriversLicense && hasGoodVision;
+
+// if (hasDriversLicense && hasGoodVision) {
+//     console.log("Sarah is able to drive!");
+// } else {
+//     console.log("Someone else should drive...");
+// }
+
+const isTired = true; // C
+
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log("Sarah is able to drive!");
+} else {
+    console.log("Someone else should drive...");
+}
+
